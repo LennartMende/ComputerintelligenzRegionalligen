3e01ffa9f2933e1159753145e7961820cf3a24d0
LennartMende <166758749+LennartMende@users.noreply.github.com>
2026-05-31 17:55:39 +0200

    FitnessCalculator docstrings added

--- src/FitnessCalculator.py
+++ src/FitnessCalculator.py
@@ -3,14 +3,17 @@ from ClubData import ClubData
 class FitnessCalculator:
     @staticmethod
     def clubs_to_coords(id_list: list[int]) -> list[tuple[float, float]]:
+        """Converts a list of club IDs to a list of their corresponding coordinates."""
         return [ClubData.clubs[id] for id in id_list]
 
     @staticmethod
     def _dist(a: tuple[float, float], b: tuple[float, float]) -> float:
+        """Calculates the Euclidean distance between two points a and b."""
         return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
     
     @staticmethod
     def _fitness_for_one_club(list_of_clubs: list[tuple[float, float]], index) -> float:
+        """Calculates the fitness for one club by summing the distances to all other clubs in the same league."""
         league = index // 20
         fitness = 0
         for i in range(league * 20, league * 20 + 20):
@@ -20,6 +23,7 @@ class FitnessCalculator:
 
     @staticmethod
     def individual_fitness(list_of_clubs) -> float:
+        """Calculates the total fitness for an individual by summing the fitness for each club."""
         if all(isinstance(club, int) for club in list_of_clubs): # club : list[int]
             list_of_clubs = FitnessCalculator.clubs_to_coords(list_of_clubs)
         elif all(isinstance(club, tuple) for club in list_of_clubs) \
