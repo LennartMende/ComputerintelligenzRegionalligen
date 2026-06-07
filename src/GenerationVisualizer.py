2339b1cb8aa09e52a93124ce08ad1a3cb57b1169
LennartMende <166758749+LennartMende@users.noreply.github.com>
2026-06-07 18:02:31 +0200

    Mutation added

---
+++ src/GenerationVisualizer.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class EAVisualizer:
+    def __init__(self, population: Population):
+        self.population = population
+    
+    @staticmethod
+    def show_avg_fit(populations: list[Population]):
+        '''Visualize the avg fitness of all generations of the population'''
+        avg_fits = np.array([pop.avg_fitness for pop in populations])
+        print("type(avg_fits): ", type(avg_fits), ", len(avg_fits): ", len(avg_fits))
+        gens = np.linspace(0, populations[-1].generation, populations[-1].generation)
+        print("type(gens): ", type(gens), ", len(gens): ", len(gens))
+        plt.plot(gens, avg_fits)
+        plt.show()
+        
+    @staticmethod
+    def show_best_fit(populations: list[Population]):
+        '''Visualize the best point's fitness of all generations of the population'''
+        best_fits = np.array([pop.best_individual.fitness for pop in populations])
+        print("type(best_fits): ", type(best_fits), ", len(best_fits): ", len(best_fits))
+        gens = np.linspace(0, populations[-1].generation, populations[-1].generation)
+        print("type(gens): ", type(gens), ", len(gens): ", len(gens))
+        plt.plot(gens, best_fits)
+        plt.show()
--- src/Individual.py
+++ src/Individual.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 import random
+from random import randint
 
 from src.ClubData import ClubData
 from src. FitnessCalculator import FitnessCalculator
 
 class Individual:
     def __init__(self):
+        self.club_ids = list(ClubData.club_coords.keys())   # club_ids is a list of integers from 1 to 80 that were used as keys in the club_coords dictionary in ClubData.py
         self.permutation = self._create_individual()
+        self.permutation_size = 80
 
     def _create_individual(self) -> list[int]:
         """Creates a random individual (a random permutation of the clubs)."""
@@ -27,3 +30,17 @@ class Individual:
         '''Calculate the fitness of an individual'''
         fitness = FitnessCalculator.individual_fitness(self.permutation)
         return fitness
+    
+    def mutation(self, mutation_swaps: int) -> None:
+        if mutation_swaps > 40 or mutation_swaps < 0:
+            raise ValueError("mutation_swaps must be between 0 and 40!")
+        self.permutation = self.permutation.copy()
+        indexes = [i for i in range(self.permutation_size)] # indexes: all unused indexes of the population
+        for _ in range (mutation_swaps):
+            index1 = randint(0, self.permutation_size - 1)
+            indexes.remove(index1)
+            index2 = randint(0, self.permutation_size - 1)
+            while index2 not in indexes:
+                index2 = randint(0, self.permutation_size - 1)
+            indexes.remove(index2)
+            self.permutation[index1], self.permutation[index2] = self.permutation[index2], self.permutation[index1]
